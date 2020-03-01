5fb39e980ca8748eba74b56bb4b4476c187ba24a
Lucas Calebe <calebematos@gmail.com>
2020-03-01 18:15:15 -0300

    Adicionando endereco ao restaurante model

--- src/main/java/com/calebematos/algafood/core/modelMapper/ModelMapperConfig.java
+++ src/main/java/com/calebematos/algafood/core/modelMapper/ModelMapperConfig.java
@@ -4,11 +4,25 @@ import org.modelmapper.ModelMapper;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import com.calebematos.algafood.api.model.EnderecoModel;
+import com.calebematos.algafood.domain.model.Endereco;
+
 @Configuration
 public class ModelMapperConfig {
 
 	@Bean
 	public ModelMapper modelMapper() {
-		return new ModelMapper();
+		var modelMapper = new ModelMapper();
+		
+		var enderecoMap = modelMapper.createTypeMap(Endereco.class, EnderecoModel.class);
+		
+		enderecoMap.<String>addMapping(endereceSrc -> endereceSrc.getCidade().getNome(), 
+				(enderecoDest, value) -> enderecoDest.setCidade(value));
+		
+		enderecoMap.<String>addMapping(endereceSrc -> endereceSrc.getCidade().getEstado().getNome(), 
+				(enderecoDest, value) -> enderecoDest.setEstado(value));
+		
+		
+		return modelMapper;
 	}
 }
