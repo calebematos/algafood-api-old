8f42d4920d5f3d4d8ec0e09a4ca3d794b36f95ef
Lucas Calebe <calebematos@gmail.com>
2020-03-28 11:03:50 -0300

    Selecionando os endpoints da API para gerar a documentação

--- src/main/java/com/calebematos/algafood/core/openapi/SpringFoxConfig.java
+++ src/main/java/com/calebematos/algafood/core/openapi/SpringFoxConfig.java
@@ -1,5 +1,6 @@
 package com.calebematos.algafood.core.openapi;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
@@ -13,10 +14,11 @@ import springfox.documentation.swagger2.annotations.EnableSwagger2;
 @EnableSwagger2
 public class SpringFoxConfig implements WebMvcConfigurer{
 
+	@Bean
 	public Docket apiDocket() {
 		return new Docket(DocumentationType.SWAGGER_2)
 				.select()
-					.apis(RequestHandlerSelectors.any())
+					.apis(RequestHandlerSelectors.basePackage("com.calebematos.algafood.api.controller"))
 					.build();
 	}
 	
