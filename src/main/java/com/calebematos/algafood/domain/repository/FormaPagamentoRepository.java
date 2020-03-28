9ad34edc5fe4176c391b52094c685d1112685d49
Lucas Calebe <calebematos@gmail.com>
2020-03-27 22:12:58 -0300

    Implementando requisições condicionais com Deep ETags

--- src/main/java/com/calebematos/algafood/domain/repository/FormaPagamentoRepository.java
+++ src/main/java/com/calebematos/algafood/domain/repository/FormaPagamentoRepository.java
@@ -1,7 +1,16 @@
 package com.calebematos.algafood.domain.repository;
 
+import java.time.OffsetDateTime;
+
+import org.springframework.data.jpa.repository.Query;
+
 import com.calebematos.algafood.domain.model.FormaPagamento;
 
 public interface FormaPagamentoRepository extends CustomJpaRepository<FormaPagamento, Long>{
 
+	@Query("select max(dataAtualizacao) from FormaPagamento")
+	OffsetDateTime getUltimaAtualizacao();
+
+	@Query("select dataAtualizacao from FormaPagamento where id = :formaPagamentoId")
+	OffsetDateTime getUltimaAtualizacaoById(Long formaPagamentoId);
 }
