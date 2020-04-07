316c18d5fccb6153dc725a3055b0e3da6a1a3019
Lucas Calebe <calebematos@gmail.com>
2020-04-07 19:33:39 -0300

    adicionando links para recurso de foto de produto

--- src/main/java/com/calebematos/algafood/api/assembler/FotoProdutoModelAssembler.java
+++ src/main/java/com/calebematos/algafood/api/assembler/FotoProdutoModelAssembler.java
@@ -2,19 +2,38 @@ package com.calebematos.algafood.api.assembler;
 
 import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.hateoas.server.mvc.RepresentationModelAssemblerSupport;
 import org.springframework.stereotype.Component;
 
+import com.calebematos.algafood.api.AlgaLinks;
+import com.calebematos.algafood.api.controller.RestauranteProdutoFotoController;
 import com.calebematos.algafood.api.model.FotoProdutoModel;
 import com.calebematos.algafood.domain.model.FotoProduto;
 
 @Component
-public class FotoProdutoModelAssembler {
+public class FotoProdutoModelAssembler extends RepresentationModelAssemblerSupport<FotoProduto, FotoProdutoModel>{
 
 	@Autowired
 	private ModelMapper modelMapper;
 	
-	public FotoProdutoModel toModel(FotoProduto foto) {
-		return modelMapper.map(foto, FotoProdutoModel.class);
+	@Autowired
+	private AlgaLinks algaLinks;
+	
+	public FotoProdutoModelAssembler() {
+		super(RestauranteProdutoFotoController.class, FotoProdutoModel.class);
+	}
+	
+	   @Override
+	    public FotoProdutoModel toModel(FotoProduto foto) {
+	        FotoProdutoModel fotoProdutoModel = modelMapper.map(foto, FotoProdutoModel.class);
+	        
+	        fotoProdutoModel.add(algaLinks.linkToFotoProduto(
+	                foto.getRestauranteId(), foto.getProduto().getId()));
+	        
+	        fotoProdutoModel.add(algaLinks.linkToProduto(
+	                foto.getRestauranteId(), foto.getProduto().getId(), "produto"));
+	        
+	        return fotoProdutoModel;
 	}
 	
 }
